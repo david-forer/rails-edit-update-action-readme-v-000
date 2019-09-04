6193c742d9cd71779db45ea037c34ac0e27fccc2
David Forer <david.j.forer@gmail.com>
2019-09-04 03:48:25 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,7 +24,6 @@ class ArticlesController < ApplicationController
   end
   
   def update 
-         def update
       @article = Article.find(params[:id])
       @article.update(title: params[:article][:title], description: params[:article][:description])
       redirect_to article_path(@article)
